df1f8a7410876cd29b9978184459b8403374f0c7
mbecker20 <49575486+mbecker20@users.noreply.github.com>
2019-07-22 17:41:41 -0400

    Update collision.py

--- collision.py
+++ collision.py
@@ -8,8 +8,7 @@ def get_delt_v(m,I_inv,r_c,vi,Li):
     r_c) matrix rep of cross prod of point of collision relative to com. ,
     vi) initial com velocity
     Li) initial ang mom about com
-    Ei) initial energy
-    Elc) energy loss coeff in [0:1], Ei*Elc=Ef"""
+    """
     a=0.5*(m+(m**2)*np.dot(I_inv,r_c).transpose()[2].dot(r_c.transpose()[2]))
     b=m*vi[2]+0.5*m*(np.dot(I_inv,Li).dot(r_c.transpose()[2])+np.dot(I_inv,r_c).transpose()[2].dot(Li))
     c=0
